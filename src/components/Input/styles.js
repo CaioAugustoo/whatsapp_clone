0a9b1d6ab0eeed0164e512265aa863670de244c8
caioaugustoo <caioamfr@gmail.com>
2021-03-27 13:40:56 -0300

    Criado componente de input

---
+++ src/components/Input/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  background: #fff;
+  border-radius: 100px;
+  padding: 10px 10px 10px 15px;
+  border: none;
+  width: 100%;
+  font-size: 1.3rem;
+`;
